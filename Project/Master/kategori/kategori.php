8204c889e2c18cd41ed951e0cb4edb30838ed7d7
micshanwid7 <michaelshan077@gmail.com>
2020-03-13 08:20:08 +0700

    ubah folder jquery

--- Project/Master/kategori/kategori.php
+++ Project/Master/kategori/kategori.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/Project-SDP/Project/jQuery/jQuery.js"></script>
+    <script src="jQuery/jQuery.js"></script>
     <title>Kategori</title>
     
 </head>
