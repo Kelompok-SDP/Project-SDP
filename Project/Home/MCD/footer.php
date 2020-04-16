2e7afcc879c80c72be548b2b797f0a59405da2d0
Fuzors-f <54881946+Fuzors-f@users.noreply.github.com>
2020-04-16 21:45:49 +0700

    footer.php

--- Project/Home/MCD/footer.php
+++ Project/Home/MCD/footer.php
@@ -4,51 +4,54 @@
         <div class="container">
             <div class="row footer-list">
                 <div class="col-md-7 col-lg-4 col-xl-5">
-                    <h5 class="footer-title">Uwenak Restoran</h5>
+                    <h5 class="footer-title" >Uwenak Restoran</h5>
                     <div class="footer-item mb-md-down-4">
                         <div class="row">
                             <div class="col-md-5">
-                                <ul class="footer-links">
-                                    <li><a href="https://mcdonalds.co.id/contact">Hubungi Kami</a></li>
+                               
+                    <ul class="footer-links">
+                                    <li><a href="https://mcdonalds.co.id/contact">Kode Promo</a></li>
                                     <li><a href="https://mcdonalds.co.id/about">Tentang Kami</a></li>
-                                    <li><a href="https://mcdonalds.co.id/newsroom">Newsroom</a></li>
-                                    <li><a href="https://mcdonalds.co.id/career">Karier</a></li>
-                                    <!-- <li><a href="/terms-condition">Syarat & Ketentuan</a></li> -->
-                                </ul>
-                            </div>
-                            <div class="col-md-7">
-                                <ul class="footer-links">
-                                    <!-- <li><a href="/news">What's On</a></li> -->
-                                    <li><a href="https://mcdonalds.co.id/services">Layanan</a></li>
-                                    <li><a href="https://mcdonalds.co.id/food-safety">Sertifikasi &amp; Jaminan Kualitas</a></li>
-                                    <li><a href="https://mcdonalds.co.id/csr">CSR</a></li>
-                                </ul>
+                             </ul>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-5 col-lg-3 col-xl-3">
                     <div class="footer-item">
-                        <h5 class="footer-title">Hubungi Kami</h5>
-                        <ul class="footer-social">
-                            <li><a href="http://www.facebook.com/McDonaldsID" target="_blank" class="social-fb"><i class="fab fa-facebook-square"></i></a></li>
-                            <li><a href="https://twitter.com/mcdonalds_id" target="_blank" class="social-twitter"><i class="fab fa-twitter-square"></i></a></li>
-                            <li><a href="http://instagram.com/mcdonaldsid" target="_blank" class="social-instagram"><i class="fab fa-instagram"></i></a></li>
-                            <li><a href="https://www.youtube.com/user/McDonaldsID" target="_blank" class="social-youtube"><i class="fab fa-youtube"></i></a></li>
-                        </ul>
-                        <div class="btn-group-download">
-                            <a href="https://apps.apple.com/id/app/mcdonalds/id1217507712" target="_blank">
-                                <img src="Mcd/Home%20%20%20McDonald's%20Indonesia_files/button_appstore.png" class="img-fluid" alt="Play Store">
-                            </a>
-                            <a href="https://play.google.com/store/apps/details?id=com.mcdonalds.mobileapp" target="_blank">
-                                <img src="Mcd/Home%20%20%20McDonald's%20Indonesia_files/button_playstore.png" class="img-fluid" alt="Google Store">
-                            </a>
-                        </div>
+                    <h5 class="footer-title">`Tak Kenal maka tak sayang- Cek Our Sosmed~ Yuk!`</h5>
+                    <ul class="footer-social">
+                                    <li><a href="http://www.facebook.com/McDonaldsID" target="_blank" class="social-fb"><i class="fab fa-facebook-square"></i></a></li>
+                                    <li><a href="https://twitter.com/mcdonalds_id" target="_blank" class="social-twitter"><i class="fab fa-twitter-square"></i></a></li>
+                                    <li><a href="http://instagram.com/mcdonaldsid" target="_blank" class="social-instagram"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="https://www.youtube.com/user/McDonaldsID" target="_blank" class="social-youtube"><i class="fab fa-youtube"></i></a></li>
+                      </ul>
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-3 col-xl-4">
+                    <div class="footer-item footer-item-subscribe">
+                    <h5 class="footer-title">Kritik dan Saran</h5>
+                        <form class="validation-footer" novalidate="">
+                            <div class="form-group mb-2">
+                                <div class="input-group">
+                                    <input type="email" class="form-control" placeholder="Kritik dan Saran" required="" name="saran" id="subscribe-footer" >
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary btn-submit btn-submit-footer sbscrb-tag" type="submit" id="button-addon-footer">
+                                           Kirim
+                                        </button>
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        Harap masukkan Saran anda
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="form-status status-footer"></p>
+                        </form>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-5 col-xl-4">
                     <div class="footer-item footer-item-subscribe">
-                        <h5 class="footer-title">Ingin berlangganan dengan kami ?</h5>
+                        <h5 class="footer-title">Ingin Menjadi Member kami ?</h5>
                         <form class="validation-footer" novalidate="">
                             <div class="form-group mb-2">
                                 <div class="input-group">
@@ -65,20 +68,28 @@
                             </div>
                             <p class="form-status status-footer"></p>
                         </form>
+                        <div style="width: 0;
+      height: 0;
+      border-top: 100px solid red;
+      border-left: 100px solid transparent; width: 10vw; position: absolute; top:8.2vh;
+ left:35vw; background-color:white;"></div>
+                        <div style="height:100px; width: 50vw; position: absolute; top:8.2vh;
+ left:45vw; background-color:red;"><h1 style="font-size:50px; padding-top:15px; margin-left:-60px;">Hope Y'ALL ENJOY Y MEAL</h1></div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
+   
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
                 <div class="col-md">
                     <ul class="text-md-right">
                          <li><a href="">+62-85-00000</a></li>
+                         <li><a href="">UwenakResto@ciamik.com</a></li>
                          <li><a href="">Jl Untung Suropati no 2B</a></li> 
-                        <li><a href="https://mcdonalds.co.id/terms-conditions">Syarat &amp; Ketentuan</a></li>
-                        <li><a href="https://mcdonalds.co.id/privacy-policy">Kebijakan Privasi</a></li>
                     </ul>
                 </div>
                 <div class="col-md order-md-first">
