83d9bb1a41731dd7bb5b6cce1611fb928f3aeb0a
micshanwid7 <michaelshan077@gmail.com>
2020-04-23 21:46:03 +0700

    Update mreport sementara

--- Project/Master/Report/Laporan_Hari/query_hjual.php
+++ Project/Master/Report/Laporan_Hari/query_hjual.php
@@ -2,10 +2,20 @@
     include("../../../config.php");
     $date=$_POST["date"];
     $total = 0;
+    $totalangka = 0;
     $query = "SELECT * FROM HJUAL where tanggal_transaksi='$date'";
     $htrans= mysqli_query($conn,$query);
     $row = mysqli_num_rows($htrans);
+    echo "<br>";
+    echo "<br>";
     if($row > 0){
+        foreach ($htrans as $key => $value3) {
+            $total = $total + $value3["total"];
+        }
+        $totalangka = $total;
+        $hasil_rupiah2 = "Rp " . number_format($totalangka,2,',','.');
+        echo "<h4>Total Pendapatan : $hasil_rupiah2</h4>";
+        
     echo "<table class='table table-bordered text-nowrap' style='margin-top:5vh;'>";
             echo "<thead>";
                 echo "<th>Tanggal Transaksi</th>";
@@ -21,7 +31,6 @@
             $angka = $value["total"];
             $hasil_rupiah = "Rp " . number_format($angka,2,',','.');
             echo "<td>$hasil_rupiah</td>";
-            $total = $total + $value["total"];
             echo "<td>$value[jenis_pemesanan]</td>";
             $query2 = "SELECT * FROM PEGAWAI WHERE ID_PEGAWAI = '$id'";
             $hasil = mysqli_query($conn,$query2);
@@ -35,17 +44,11 @@
             echo "</tr>";
         }
     echo "</table>";
-    }
-    echo "<br>";
-    $totalangka = $total;
-    if($totalangka == 0){
+    }else{
         echo "<p style='margin-left:25vw;color:grey; font-style:italic;'>Tidak ada hasil record</p>";
         echo "<h4>Total Pendapatan : -</h4>";
-    }else{
-        $hasil_rupiah2 = "Rp " . number_format($totalangka,2,',','.');
-        echo "<h4>Total Pendapatan : $hasil_rupiah2</h4>";
-        echo "<br>";
     }
+    
 ?>
 <script>
     function detail(id){
---
+++ Project/Master/Report/Menu_Favorite_Hari/query_menu.php
@@ -0,0 +1,45 @@
+<?php
+    include("../../../config.php");
+    $date=$_POST["date"];
+    $query = "SELECT * FROM HJUAL WHERE TANGGAL_TRANSAKSI = '$date'";
+    $htrans= mysqli_query($conn,$query);
+    $row = mysqli_num_rows($htrans);
+    if($row > 0){
+        foreach ($htrans as $key => $value) {
+            $id = $value["id_hjual"];
+            $query2 = "SELECT SUM(jumlah) as jumlahmenu, COUNT(ID_MENU) as jumlah2, id_menu as id FROM DJUAL WHERE ID_HJUAL = '$id'";
+            $dtrans= mysqli_query($conn,$query2);
+            foreach ($dtrans as $key => $value2) {
+                $tmp[] = $value2["jumlahmenu"];
+                $tmp2[] = $value2["id"];       
+                $tmp3[] = $value2["jumlah2"];       
+            }      
+        }
+        $mak = max($tmp);
+        $idx = 0;
+        for ($i=0; $i < count($tmp); $i++) { 
+            if($tmp[$i] == $mak){
+                $idx = $i;
+            }
+        }
+        $idm = $tmp2[$idx];
+        $query3 = "SELECT * FROM MENU WHERE ID_MENU = '$idm'";
+        $menu = mysqli_query($conn,$query3);
+        echo "<table class='table table-bordered text-nowrap' style='margin-top:5vh;'>";
+            echo "<thead>";
+                echo "<th>Gambar</th>";
+                echo "<th>Nama Menu</th>";
+                echo "<th>Jumlah Dipesan</th>";
+                echo "<th>Total Pesanan</th>";
+            echo "</thead>";
+            foreach ($menu as $key => $value3) {
+                echo "<tr>";
+                    echo "<td><img src='Menu/$value3[gambar]' alt='' style='max-width:100px;max-height:100px'></td>";
+                    echo "<td>$value3[nama_menu]</td>";
+                    echo "<td>$tmp3[$idx]</td>";
+                    echo "<td>$tmp[$idx]</td>";
+                    echo "</tr>";
+            }
+        echo "</table>";
+    }
+?>
