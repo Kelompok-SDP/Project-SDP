b14cc0a0acdc5cb0ac24a3dfda3b9781ccaa7407
micshanwid7 <michaelshan077@gmail.com>
2020-05-02 12:30:51 +0700

    report

--- Project/Master/Report.php
+++ Project/Master/Report.php
@@ -12,7 +12,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  
+  <link href='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css'>
 </head>
 <style>
 .sticky{
@@ -395,4 +395,12 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
+<script>
+  $(() => {
+    $('#month').datepicker();
+  });
 
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
