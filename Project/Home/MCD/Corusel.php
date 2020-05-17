0722cf925c72aa85604f4d427532713d498cb1fb
micshanwid7 <michaelshan077@gmail.com>
2020-05-17 15:09:49 +0700

    Home kupon

--- Project/Home/MCD/Corusel.php
+++ Project/Home/MCD/Corusel.php
@@ -1,36 +1,43 @@
-
 <div class="section-cover-home animated fadeInUp">
     <div id="carouselCoverResponsive" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators animated fadeInUp delayp3">
             <li data-target="#carouselCoverResponsive" data-slide-to="0" class="active"></li>
             <li data-target="#carouselCoverResponsive" data-slide-to="1" class=""></li>
+            <li data-target="#carouselCoverResponsive" data-slide-to="2" class=""></li>
         </ol>
         <div class='carousel-inner'>
         <?php
         $datenow = date('Y-m-d');
         $monthnow = date('m');
-        $query = "SELECT * from promo where periode_akhir >= '$datenow' AND periode_akhir like '%$monthnow%' AND status_promo = 1 ORDER BY RAND() LIMIT 2";
+        $query = "SELECT * from promo where periode_akhir >= '$datenow' AND periode_akhir like '%$monthnow%' AND status_promo = 1 ORDER BY RAND() LIMIT 3";
         $menu=mysqli_query($conn,$query);
         echo $query;
         $row = mysqli_num_rows($menu);
         if($row > 0){
         $ctr=0;
-        foreach ($menu as $key => $value) {
-            if($ctr==0){
-                echo "<div class='carousel-item active carousel-item-left'>";
-                    echo " <a href='#' target=''>";
-                    echo "<img src='../master/promo/$value[gambar_promo]' style='background-size: cover;100%;height:700px' class='img-fluid w-100 d-none d-sm-block'>";
-                    echo "</a>";
-                echo "</div>";
-                $ctr=1;
-            }else{
-                echo "<div class='carousel-item carousel-item-next carousel-item-left'>";
-                    echo " <a href='#' target=''>";
-                    echo "<img src='../master/promo/$value[gambar_promo]' style='background-size: cover;100%;height:700px' class='img-fluid w-100 d-none d-sm-block'>";
-                    echo "</a>";
-                echo "</div>";
+            foreach ($menu as $key => $value) {
+                if($ctr==0){
+                    echo "<div class='carousel-item active carousel-item-left'>";
+                        echo " <a href='#' target=''>";
+                        echo "<img src='../master/promo/$value[gambar_promo]' style='background-size: cover;100%;height:700px' class='img-fluid w-100 d-none d-sm-block'>";
+                        echo "</a>";
+                    echo "</div>";
+                    
+                }else if($ctr == 1){
+                    echo "<div class='carousel-item carousel-item-next carousel-item-left'>";
+                        echo " <a href='#' target=''>";
+                        echo "<img src='../master/promo/$value[gambar_promo]' style='background-size: cover;100%;height:700px' class='img-fluid w-100 d-none d-sm-block'>";
+                        echo "</a>";
+                    echo "</div>";
+                }else{
+                    echo "<div class='carousel-item carousel-item-next carousel-item-left'>";
+                        echo " <a href='#' target=''>";
+                        echo "<img src='../master/promo/$value[gambar_promo]' style='background-size: cover;100%;height:700px' class='img-fluid w-100 d-none d-sm-block'>";
+                        echo "</a>";
+                    echo "</div>";
+                }
+                $ctr++;
             }
-        }
         }else{
             echo "Kosong";
         }
--- Project/Master/Edit_Menu.php
+++ Project/Master/Edit_Menu.php
@@ -59,6 +59,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+<?php include("../sidebar.php"); ?>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
  
